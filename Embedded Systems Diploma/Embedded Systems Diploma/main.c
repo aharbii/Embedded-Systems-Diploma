a1cbf6edcaa7adcdef967c3b75aee33ff0340e32
Ahmed Harbi <ahmed.harbi.eg@gmail.com>
2022-02-04 17:58:38 +0200

    ARRAYS

---
+++ Embedded Systems Diploma/Embedded Systems Diploma/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  Embedded Systems Diploma
+//
+//  Created by Ahmed Harbi on 01/02/2022.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
