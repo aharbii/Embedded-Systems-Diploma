c20a19dc5d11568c7842620e2acd764f1bdeda94
Ahmed Harbi <ahmed.harbi.eg@gmail.com>
2022-02-25 18:44:17 +0200

    Pattern

--- C Programming/pattern.c
+++ C Programming/pattern.c
@@ -1,36 +1,35 @@
-int fun(int x,int y)
+int fun(int x, int y)
 {
-    if(x<0)
+    if (x < 0)
     {
-        x=x*-1;
+        x = x * -1;
     }
-    if(y<0)
+    if (y < 0)
     {
-        y=y*-1;
+        y = y * -1;
     }
-    if(x>y)
+    if (x > y)
         return x;
     else
         return y;
 }
 int main(void)
 {
-  int n,y,x,r,num,m,i;
- while(1)
+    int n, y, x, r, num, m, i;
+    while (1)
     {
-        scanf("%d",&num);
+        scanf("%d", &num);
 
-        n=num*2+1;
-            for(y=-1*num; y<=num; y=y+1)
+        n = num * 2 + 1;
+        for (y = -1 * num; y <= num; y = y + 1)
+        {
+            for (x = -1 * num; x <= num; x++)
             {
-                for(x=-1*num; x<=num; x++)
-                {
 
-                        printf("%d",fun(x,y));
-
-                }
-                printf("\n");
+                printf("%d", fun(x, y));
             }
+            printf("\n");
         }
-		return 0;
-		}
+    }
+    return 0;
+}
